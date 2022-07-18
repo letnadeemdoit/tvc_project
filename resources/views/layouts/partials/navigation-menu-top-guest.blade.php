efa8b21c65cf72983ee41caa6e0e9ec559e79589
Ahmad_Dev <105263889+Ahmad-DevDimension@users.noreply.github.com>
2022-07-18 14:37:17 +0500

    ....

--- resources/views/layouts/partials/navigation-menu-top-guest.blade.php
+++ resources/views/layouts/partials/navigation-menu-top-guest.blade.php
@@ -33,80 +33,47 @@
 
 <header id="header" class="navbar navbar-expand-lg navbar-bordered bg-white  ">
     <div class="container">
-
+        <!--   nav     -->
         <nav class="js-mega-menu navbar-nav-wrap d-flex justify-content-center">
-            <!-- Logo -->
-
-            <a class="navbar-brand" href="{{ url('/') }}" aria-label="Front">
-                <img
-                    class="navbar-brand-logo"
-                    src="{{ asset('logo/logo.png') }}"
-                    alt="Logo" data-hs-theme-appearance="default"
-                />
-                {{--                <img class="navbar-brand-logo" src="../assets/svg/logos-light/logo.svg" alt="Logo" data-hs-theme-appearance="dark">--}}
+            <a class="navbar-brand" href="#">
+                <img   class="navbar-brand-logo"
+                       src="<?php echo e(asset('logo/logo.png')); ?>"
+                       alt="Logo" data-hs-theme-appearance="default" />
             </a>
 
-            <!-- End Logo -->
-
-            <!-- Secondary Content -->
-            <div class="navbar-nav-wrap-secondary-content">
-                <!-- Navbar -->
-                <ul class="navbar-nav fs-3">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}" data-placement="left">
-                            <i class="bi-book bi-box-arrow-right"></i> Signup
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}" data-placement="left">
-                            <i class="bi-book bi-box-arrow-in-right"></i> Signin
-                        </a>
-                    </li>
-                </ul>
-                <!-- End Navbar -->
-            </div>
-            <!-- End Secondary Content -->
-
-            <!-- Toggler -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarContainerNavDropdown" aria-controls="navbarContainerNavDropdown"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-default">
-                    <i class="bi-list"></i>
-                </span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="#navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-default">
+        <i class="bi-list"></i>
+    </span>
                 <span class="navbar-toggler-toggled">
-                    <i class="bi-x"></i>
-                </span>
+        <i class="bi-x"></i>
+    </span>
             </button>
-            <!-- End Toggler -->
 
-            <!-- Collapse -->
-            <div class="collapse navbar-collapse" id="navbarContainerNavDropdown">
-                <ul class="navbar-nav fs-3">
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav  mb-2 mb-lg-0 mx-auto d-flex justify-content-center">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/') }}" data-placement="left">
-                            <i class="bi-book bi-house-fill"></i> Home
-                        </a>
+                        <a class="nav-link active" aria-current="page" href="./index.html">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('bulletin-board') }}" data-placement="left">
-                            <i class="bi-book bi-clipboard-data"></i> Bulletin Board
-                        </a>
+                        <a class="nav-link" href="/">Bulletin Board</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('guest.contact') }}" data-placement="left">
-                            <i class="bi-book bi-envelope-fill"></i> Contact Us
-                        </a>
+                        <a class="nav-link" href="/" tabindex="-1">CONTACT US</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/') }}" data-placement="left">
-                            <i class="bi-book bi-journal-album"></i> Albums
-                        </a>
+                        <a class="nav-link" href="/" tabindex="-1">Albums</a>
                     </li>
+
+
                 </ul>
-
+                <div class="d-block d-md-flex nav-buttons">
+                    <a href="/" class="btn btn-outline-dark px-md-5 px-lg-4 px-xl-5" type="submit">SIGN UP</a>
+                    <a href="/" class="btn btn-outline bg-primary px-md-5 px-lg-4 px-xl-5 ms-3 text-white" type="submit">LOGIN</a>
+                </div>
             </div>
-            <!-- End Collapse -->
         </nav>
+        <!--    ends    -->
+
     </div>
 </header>
