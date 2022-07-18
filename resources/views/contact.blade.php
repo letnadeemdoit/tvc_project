335b7298664fca22c81c7148869ae00044158617
Ahmad_Dev <105263889+Ahmad-DevDimension@users.noreply.github.com>
2022-07-18 18:38:55 +0500

    ....

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,97 +9,103 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 mx-auto">
-        <div class="contact-text shadow-1-strong rounded  d-flex justify-content-center mt-5">
-            <h1 class="text-primary font-jost">Find Your Vacation House</h1>
-        </div>
-        <h3 class="pt-2">We’d Love to Hear From You</h3>
-        <p class="pt-2">Thank you very much for visiting TheVacationCalendar.com. If you have any questions about the service, technical issues, or any other suggestions, please feel free to email.</p>
-        </div>
+                    <div class="contact-text shadow-1-strong rounded  d-flex justify-content-center mt-5">
+                        <h1 class="text-primary font-jost">Find Your Vacation House</h1>
+                    </div>
+                    <h3 class="pt-2">We’d Love to Hear From You</h3>
+                    <p class="pt-2">Thank you very much for visiting TheVacationCalendar.com. If you have any questions about the service, technical issues, or any other suggestions, please feel free to email.</p>
+                </div>
             </div>
         </div>
     </section>
 
     <!-- Contact Form -->
-    <div class="container content-space-2 content-space-lg-3">
-        <div class="row">
-            <div class="col-md-6">
-                <!-- Card -->
-                <div class="card shadow-lg">
-                    <div class="card-body p-6">
-                        <!-- Heading -->
-                        <div class="text-center mb-5 mb-md-9">
-                            <h2 class="display-5">Contact Form</h2>
-                            <p>Whether you have questions or you would just like to say hello, contact us.</p>
-                        </div>
-                        <!-- End Heading -->
-
-                        <!-- Form -->
-                        <form>
-                            <div class="row gx-3">
-                                <div class="col-sm-6">
-                                    <!-- Form -->
-                                    <div class="mb-3">
-                                        <label class="form-label" for="hireUsFormFirstName">First name</label>
-                                        <input type="text" class="form-control form-control-lg" name="hireUsFormNameFirstName" id="hireUsFormFirstName" placeholder="First name" aria-label="First name">
-                                    </div>
-                                    <!-- End Form -->
-                                </div>
-                                <!-- End Col -->
-
-                                <div class="col-sm-6">
-                                    <!-- Form -->
-                                    <div class="mb-3">
-                                        <label class="form-label" for="hireUsFormLasttName">Last name</label>
-                                        <input type="text" class="form-control form-control-lg" name="hireUsFormNameLastName" id="hireUsFormLasttName" placeholder="Last name" aria-label="Last name">
-                                    </div>
-                                    <!-- End Form -->
-                                </div>
-                                <!-- End Col -->
+    <div class="contact-page">
+        <div class="container content-space-2 content-space-lg-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- Card -->
+                    <div class="card shadow-lg">
+                        <div class="card-body p-6">
+                            <!-- Heading -->
+                            <div class="text-center mb-5 mb-md-9">
+                                <h2 class="display-5">Contact Form</h2>
+                                <p>Whether you have questions or you would just like to say hello, contact us.</p>
                             </div>
-                            <!-- End Row -->
-
-                            <div class="row gx-3">
-                                <div class="col-sm-6">
-                                    <!-- Form -->
-                                    <div class="mb-3">
-                                        <label class="form-label" for="hireUsFormWorkEmail">Email address</label>
-                                        <input type="email" class="form-control form-control-lg" name="hireUsFormNameWorkEmail" id="hireUsFormWorkEmail" placeholder="email@site.com" aria-label="email@site.com">
-                                    </div>
-                                    <!-- End Form -->
-                                </div>
-                                <!-- End Col -->
-
-                                <div class="col-sm-6">
-                                    <!-- Form -->
-                                    <div class="mb-3">
-                                        <label class="form-label" for="hireUsFormPhone">Phone <span class="form-label-secondary">(Optional)</span></label>
-                                        <input type="text" class="form-control form-control-lg" name="hireUsFormNamePhone" id="hireUsFormPhone" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx">
-                                    </div>
-                                    <!-- End Form -->
-                                </div>
-                                <!-- End Col -->
-                            </div>
-                            <!-- End Row -->
+                            <!-- End Heading -->
 
                             <!-- Form -->
-                            <div class="mb-3">
-                                <label class="form-label" for="hireUsFormDetails">Details</label>
-                                <textarea class="form-control form-control-lg" name="hireUsFormNameDetails" id="hireUsFormDetails" placeholder="Tell us about your ..." aria-label="Tell us about your ..." rows="4"></textarea>
-                            </div>
-                            <!-- End Form -->
+                            <form>
+                                <div class="row gx-3">
+                                    <div class="col-sm-6">
+                                        <!-- Form -->
+                                        <div class="mb-3">
+                                            <label class="form-label" for="hireUsFormFirstName">First name</label>
+                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNameFirstName" id="hireUsFormFirstName" placeholder="First name" aria-label="First name">
+                                        </div>
+                                        <!-- End Form -->
+                                    </div>
+                                    <!-- End Col -->
 
-                            <div class="d-flex justify-content-center contact-btn">
-                                <button type="submit" class="btn btn-primary btn-lg">Send</button>
-                                <button type="submit" class="btn btn-secondary btn-lg ms-3">Reset</button>
-                            </div>
-                        </form>
-                        <!-- End Form -->
+                                    <div class="col-sm-6">
+                                        <!-- Form -->
+                                        <div class="mb-3">
+                                            <label class="form-label" for="hireUsFormLasttName">Last name</label>
+                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNameLastName" id="hireUsFormLasttName" placeholder="Last name" aria-label="Last name">
+                                        </div>
+                                        <!-- End Form -->
+                                    </div>
+                                    <!-- End Col -->
+                                </div>
+                                <!-- End Row -->
+
+                                <div class="row gx-3">
+                                    <div class="col-sm-6">
+                                        <!-- Form -->
+                                        <div class="mb-3">
+                                            <label class="form-label" for="hireUsFormWorkEmail">Email address</label>
+                                            <input type="email" class="form-control form-control-lg" name="hireUsFormNameWorkEmail" id="hireUsFormWorkEmail" placeholder="email@site.com" aria-label="email@site.com">
+                                        </div>
+                                        <!-- End Form -->
+                                    </div>
+                                    <!-- End Col -->
+
+                                    <div class="col-sm-6">
+                                        <!-- Form -->
+                                        <div class="mb-3">
+                                            <label class="form-label" for="hireUsFormPhone">Phone <span class="form-label-secondary">(Optional)</span></label>
+                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNamePhone" id="hireUsFormPhone" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx">
+                                        </div>
+                                        <!-- End Form -->
+                                    </div>
+                                    <!-- End Col -->
+                                </div>
+                                <!-- End Row -->
+
+                                <!-- Form -->
+                                <div class="mb-3">
+                                    <label class="form-label" for="hireUsFormDetails">Details</label>
+                                    <textarea class="form-control form-control-lg" name="hireUsFormNameDetails" id="hireUsFormDetails" placeholder="Tell us about your ..." aria-label="Tell us about your ..." rows="4"></textarea>
+                                </div>
+                                <!-- End Form -->
+
+                                <div class="d-flex justify-content-center contact-btn">
+                                    <button type="submit" class="btn btn-primary btn-lg">Send</button>
+                                    <button type="submit" class="btn btn-secondary btn-lg ms-3">Reset</button>
+                                </div>
+                            </form>
+                            <!-- End Form -->
+                        </div>
+                    </div>
+                    <!-- End Card -->
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109132.50717458608!2d72.21911081640624!3d31.265282300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3923a3d944211f27%3A0xfe1509a2f8d6f2a5!2stravelforbes!5e0!3m2!1sen!2s!4v1655819155305!5m2!1sen!2s" class="w-100" height="570" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
                 </div>
-                <!-- End Card -->
-            </div>
-            <div class="col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109132.50717458608!2d72.21911081640624!3d31.265282300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3923a3d944211f27%3A0xfe1509a2f8d6f2a5!2stravelforbes!5e0!3m2!1sen!2s!4v1655819155305!5m2!1sen!2s" class="w-100" height="570" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,69 +1,70 @@
 <x-guest-layout>
-@push('stylesheets')
+    @push('stylesheets')
 
-@endpush
+    @endpush
 
-@include('partials.sub-page-hero-section');
-
-    <section>
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-5 text-center text-lg-start ">
-                    <!-- Jumbotron -->
-                    <div
-                        class="text-center shadow-1-strong rounded text-white privacy-policy">
-                        <h1 class="h2 text-start text-primary font-jost">Find Your Vacation House</h1>
-                    </div>
-                    <div class="pt-3">
-                        <h3>Privacy Policy</h3>
-                        <p class="pe-0 pe-lg-4">
-                            This Privacy Policy describes how your personal information is collected, used, and shared when you visit or subscribe to the www.thevacationcalendar.com (the "Site"). Please note that the Site is a hobby that charges a nominal fee to keep the project going since it addresses a niche need and many vacation home owners enjoy it. If you are concerned about privacy and data protection, please do not use the Site. The Site has basic security but should not be used to capture any information that is not publicly available or that the end users are willing to make publicly available. </p>
-                    </div>
-                </div>
-                <div class="col-lg-6 mt-4 mt-lg-0 offset-md-1 text-center">
-                  <img src="  {{asset('/images/privacy-policy/group-image.png')}}" class="img-fluid" />
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="accordion-section">
-        <div class="container">
-        <div class="m-4">
-            <div class="accordion" id="myAccordion">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">1. What is HTML?</button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
-                        <div class="card-body shadow-lg">
-                            <p>HTML stands for HyperText Markup Language. HTML is the standard markup language for describing the structure of web pages. <a href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+    @include('partials.sub-page-hero-section');
+    <div class="privacy-policy-page">
+        <section>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 text-center text-lg-start ">
+                        <!-- Jumbotron -->
+                        <div
+                            class="text-center shadow-1-strong rounded text-white privacy-policy">
+                            <h1 class="h2 text-start text-primary font-jost">Find Your Vacation House</h1>
+                        </div>
+                        <div class="pt-3">
+                            <h3>Privacy Policy</h3>
+                            <p class="pe-0 pe-lg-4">
+                                This Privacy Policy describes how your personal information is collected, used, and shared when you visit or subscribe to the www.thevacationcalendar.com (the "Site"). Please note that the Site is a hobby that charges a nominal fee to keep the project going since it addresses a niche need and many vacation home owners enjoy it. If you are concerned about privacy and data protection, please do not use the Site. The Site has basic security but should not be used to capture any information that is not publicly available or that the end users are willing to make publicly available. </p>
                         </div>
                     </div>
-                </div>
-                <div class="accordion-item mt-3">
-                    <h2 class="accordion-header" id="headingTwo">
-                        <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">2. What is Bootstrap?</button>
-                    </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
-                        <div class="card-body shadow-lg">
-                            <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item mt-3">
-                    <h2 class="accordion-header" id="headingThree">
-                        <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">3. What is CSS?</button>
-                    </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
-                        <div class="card-body shadow-lg">
-                            <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
-                        </div>
+                    <div class="col-lg-6 mt-4 mt-lg-0 offset-md-1 text-center">
+                        <img src="  {{asset('/images/privacy-policy/group-image.png')}}" class="img-fluid" />
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
-    </section>
+        </section>
 
+        <section class="accordion-section mt-80">
+            <div class="container">
+                <div class="m-4">
+                    <div class="accordion" id="myAccordion">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseOne">1. What is HTML?</button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                                <div class="card-body bottom-box-shadow">
+                                    <p>HTML stands for HyperText Markup Language. HTML is the standard markup language for describing the structure of web pages. <a href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mt-3">
+                            <h2 class="accordion-header" id="headingTwo">
+                                <button type="button" class="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseTwo">2. What is Bootstrap?</button>
+                            </h2>
+                            <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
+                                <div class="card-body bottom-box-shadow">
+                                    <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mt-3">
+                            <h2 class="accordion-header" id="headingThree">
+                                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">3. What is CSS?</button>
+                            </h2>
+                            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                                <div class="card-body bottom-box-shadow">
+                                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </x-guest-layout>
+
