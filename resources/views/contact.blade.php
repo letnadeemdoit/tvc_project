a3cbb66afa6f8f60bff8a4aaf291e0f94d7f5ce5
Ahmad_Dev <105263889+Ahmad-DevDimension@users.noreply.github.com>
2022-07-19 18:39:28 +0500

    ....

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,7 +13,8 @@
                         <h1 class="text-primary font-jost">Find Your Vacation House</h1>
                     </div>
                     <h3 class="pt-2">We’d Love to Hear From You</h3>
-                    <p class="pt-2">Thank you very much for visiting TheVacationCalendar.com. If you have any questions about the service, technical issues, or any other suggestions, please feel free to email.</p>
+                    <p class="pt-2">Thank you very much for visiting TheVacationCalendar.com. If you have any questions
+                        about the service, technical issues, or any other suggestions, please feel free to email.</p>
                 </div>
             </div>
         </div>
@@ -35,61 +36,38 @@
                             <!-- End Heading -->
 
                             <!-- Form -->
-                            <form>
-                                <div class="row gx-3">
-                                    <div class="col-sm-6">
-                                        <!-- Form -->
-                                        <div class="mb-3">
-                                            <label class="form-label" for="hireUsFormFirstName">First name</label>
-                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNameFirstName" id="hireUsFormFirstName" placeholder="First name" aria-label="First name">
-                                        </div>
-                                        <!-- End Form -->
-                                    </div>
-                                    <!-- End Col -->
-
-                                    <div class="col-sm-6">
-                                        <!-- Form -->
-                                        <div class="mb-3">
-                                            <label class="form-label" for="hireUsFormLasttName">Last name</label>
-                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNameLastName" id="hireUsFormLasttName" placeholder="Last name" aria-label="Last name">
-                                        </div>
-                                        <!-- End Form -->
-                                    </div>
-                                    <!-- End Col -->
-                                </div>
-                                <!-- End Row -->
-
-                                <div class="row gx-3">
-                                    <div class="col-sm-6">
-                                        <!-- Form -->
-                                        <div class="mb-3">
-                                            <label class="form-label" for="hireUsFormWorkEmail">Email address</label>
-                                            <input type="email" class="form-control form-control-lg" name="hireUsFormNameWorkEmail" id="hireUsFormWorkEmail" placeholder="email@site.com" aria-label="email@site.com">
-                                        </div>
-                                        <!-- End Form -->
-                                    </div>
-                                    <!-- End Col -->
-
-                                    <div class="col-sm-6">
-                                        <!-- Form -->
-                                        <div class="mb-3">
-                                            <label class="form-label" for="hireUsFormPhone">Phone <span class="form-label-secondary">(Optional)</span></label>
-                                            <input type="text" class="form-control form-control-lg" name="hireUsFormNamePhone" id="hireUsFormPhone" placeholder="+x(xxx)xxx-xx-xx" aria-label="+x(xxx)xxx-xx-xx">
-                                        </div>
-                                        <!-- End Form -->
-                                    </div>
-                                    <!-- End Col -->
-                                </div>
-                                <!-- End Row -->
-
+                            <form class="contact-us-form">
                                 <!-- Form -->
-                                <div class="mb-3">
-                                    <label class="form-label" for="hireUsFormDetails">Details</label>
-                                    <textarea class="form-control form-control-lg" name="hireUsFormNameDetails" id="hireUsFormDetails" placeholder="Tell us about your ..." aria-label="Tell us about your ..." rows="4"></textarea>
-                                </div>
-                                <!-- End Form -->
 
-                                <div class="d-flex justify-content-center contact-btn">
+                                <fieldset class="border-light">
+                                    <legend class="float-none w-auto fs-5">First Name</legend>
+                                    <input type="text" class="form-control" id="text-input-1"
+                                           placeholder="Placeholder" value="First Name"/>
+
+
+                                </fieldset>
+                                <!-- End Form -->
+                                <fieldset class="border-light mt-4">
+                                    <legend class="float-none w-auto fs-5">Last Name</legend>
+                                    <input type="text" class="form-control" id="text-input-2"
+                                           placeholder="Placeholder" value="Last Name"/>
+                                </fieldset>
+                                <fieldset class="border-light mt-4">
+                                    <legend class="float-none w-auto fs-5">Email</legend>
+                                    <input type="text" class="form-control" id="text-input-3"
+                                           placeholder="Placeholder" value="Enter E-mail"/>
+                                </fieldset>
+                                <fieldset class="border-light mt-4">
+                                    <legend class="float-none w-auto fs-5">Subject</legend>
+                                    <input type="text" class="form-control" id="text-input-4"
+                                           placeholder="Placeholder" value="Subject"/>
+                                </fieldset>
+                                <fieldset class="border-light mt-4">
+                                    <legend class="float-none w-auto fs-5">Subject</legend>
+                                    <textarea  class="form-control" id="text-input-5"
+                                           placeholder="Placeholder" value="Subject" rows="3"></textarea>
+                                </fieldset>
+                                <div class="d-block d-md-flex justify-content-center text-center contact-btn mt-4">
                                     <button type="submit" class="btn btn-primary btn-lg">Send</button>
                                     <button type="submit" class="btn btn-secondary btn-lg ms-3">Reset</button>
                                 </div>
@@ -100,9 +78,12 @@
                     <!-- End Card -->
                 </div>
                 <div class="col-md-6">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-body">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109132.50717458608!2d72.21911081640624!3d31.265282300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3923a3d944211f27%3A0xfe1509a2f8d6f2a5!2stravelforbes!5e0!3m2!1sen!2s!4v1655819155305!5m2!1sen!2s" class="w-100" height="570" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109132.50717458608!2d72.21911081640624!3d31.265282300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3923a3d944211f27%3A0xfe1509a2f8d6f2a5!2stravelforbes!5e0!3m2!1sen!2s!4v1655819155305!5m2!1sen!2s"
+                                class="w-100" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
@@ -110,7 +91,4 @@
         </div>
     </div>
     <!-- End Contact Form -->
-
-
-
 </x-guest-layout>
