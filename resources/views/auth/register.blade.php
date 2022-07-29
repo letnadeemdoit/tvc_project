4d0de5bb6c1530c20a1708d130afe625b07ce4dc
Rana Tayyab <tayyab.devdi@gmail.com>
2022-07-29 16:24:29 +0500

    fixes.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -101,18 +101,11 @@
                         @enderror
                     </div>
                     <div class="mb-2">
-
-                        <!-- Dropzone -->
                         <div id="basicExampleDropzone"
                              class="js-dropzone row dz-dropzone dz-dropzone-card border-primary bg-primary-light mx-auto">
                             <div class="dz-message">
-
-                                {{--                                <img class="avatar avatar-xl avatar-4x3 mb-3" src="../assets/svg/illustrations/oc-browse.svg" alt="Image Description">--}}
-
                                 <h5>Drag and drop your file here</h5>
-
                                 <p class="mb-2">or</p>
-
                                 <span class="btn bg-primary btn-sm text-white">Upload Image</span>
                             </div>
                         </div>
@@ -307,71 +300,17 @@
             </div>
             <!-- second fieldset ends -->
         </div>
-
-
-        {{--      ends  --}}
-        {{--        <x-jet-validation-errors class="mb-4" />--}}
-
-        {{--        <form method="POST" action="{{ route('register') }}">--}}
-        {{--            @csrf--}}
-
-        {{--            <div>--}}
-        {{--                <x-jet-label for="name" value="{{ __('Name') }}" />--}}
-        {{--                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />--}}
-        {{--            </div>--}}
-
-        {{--            <div class="mt-4">--}}
-        {{--                <x-jet-label for="email" value="{{ __('Email') }}" />--}}
-        {{--                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />--}}
-        {{--            </div>--}}
-
-        {{--            <div class="mt-4">--}}
-        {{--                <x-jet-label for="password" value="{{ __('Password') }}" />--}}
-        {{--                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />--}}
-        {{--            </div>--}}
-
-        {{--            <div class="mt-4">--}}
-        {{--                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />--}}
-        {{--                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />--}}
-        {{--            </div>--}}
-
-        {{--            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())--}}
-        {{--                <div class="mt-4">--}}
-        {{--                    <x-jet-label for="terms">--}}
-        {{--                        <div class="flex items-center">--}}
-        {{--                            <x-jet-checkbox name="terms" id="terms"/>--}}
-
-        {{--                            <div class="ml-2">--}}
-        {{--                                {!! __('I agree to the :terms_of_service and :privacy_policy', [--}}
-        {{--                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',--}}
-        {{--                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',--}}
-        {{--                                ]) !!}--}}
-        {{--                            </div>--}}
-        {{--                        </div>--}}
-        {{--                    </x-jet-label>--}}
-        {{--                </div>--}}
-        {{--            @endif--}}
-
-        {{--            <div class="flex items-center justify-end mt-4">--}}
-        {{--                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">--}}
-        {{--                    {{ __('Already registered?') }}--}}
-        {{--                </a>--}}
-
-        {{--                <x-jet-button class="ml-4">--}}
-        {{--                    {{ __('Register') }}--}}
-        {{--                </x-jet-button>--}}
-        {{--            </div>--}}
-        {{--        </form>--}}
     </x-jet-authentication-card>
 
-    @push('scripts')
-        <script>
-            $(function() {
-                console.log(window.HSCore.components);
-                // INITIALIZATION OF DROPZONE
-                // =======================================================
-                HSCore.components.HSDropzone.init('.js-dropzone');
-            });
-        </script>
-    @endpush
+{{--    @push('scripts')--}}
+{{--        <script>--}}
+{{--            $(function() {--}}
+{{--                // INITIALIZATION OF DROPZONE--}}
+{{--                // =======================================================--}}
+{{--                HSCore.components.HSDropzone.init('.js-dropzone', {--}}
+{{--                    url: "{{ route('register') }}",--}}
+{{--                });--}}
+{{--            });--}}
+{{--        </script>--}}
+{{--    @endpush--}}
 </x-auth-layout>
