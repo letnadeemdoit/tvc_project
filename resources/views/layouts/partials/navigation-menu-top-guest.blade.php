8125c8e44569ebe9d2ebfcaf6bd8571b776f8964
Nouman Afzal <noumanafzal.devdimensions.com>
2022-08-04 16:44:20 +0500

    ...

--- resources/views/layouts/partials/navigation-menu-top-guest.blade.php
+++ resources/views/layouts/partials/navigation-menu-top-guest.blade.php
@@ -6,7 +6,7 @@
                 <div class="d-flex justify-content-center justify-content-md-start">
 
                     <a href="#"> <i class="fa-brands fa-facebook-f fs-3 text-white pt-1"></i></a>
-                    <a href="#">  <i class="bi-twitter mx-3 fs-3 text-white"></i></a>
+                    <a href="#"> <i class="bi-twitter mx-3 fs-3 text-white"></i></a>
                     <a href="#"> <i class="bi-instagram fs-3 text-white"></i></a>
                 </div>
             </div>
@@ -20,7 +20,7 @@
                     </div>
                     <div
                         class="d-flex align-items-center ms-md-2 ms-4 justify-content-center justify-content-md-start">
-{{--                        <img src="{{asset('/images/images-home/Email.svg')}}" class="img-fluid me-2">--}}
+                        {{--                        <img src="{{asset('/images/images-home/Email.svg')}}" class="img-fluid me-2">--}}
                         <i class="fa-regular fa-envelope text-white me-2 fs-3"></i>
                         <a class="mb-0 text-white fs-12" href="mailto:someone@example.com">trips.calendar@gmail.com</a>
                     </div>
@@ -34,48 +34,59 @@
 <header id="header" class="navbar navbar-expand-lg navbar-bordered bg-white  ">
     <div class="container">
         <!--   nav     -->
-        <nav class="js-mega-menu navbar-nav-wrap d-flex justify-content-center">
+        <nav class="js-mega-menu navbar-nav-wrap d-flex justify-content-center py-2">
             <a class="navbar-brand" href="{{route('guest.welcome')}}">
-                <img   class="navbar-brand-logo"
-                       src="<?php echo e(asset('logo/logo.svg')); ?>"
-                       alt="Logo" data-hs-theme-appearance="default" />
+                <img class="navbar-brand-logo"
+                     src="<?php echo e(asset('logo/logo.svg')); ?>"
+                     alt="Logo" data-hs-theme-appearance="default"/>
             </a>
 
-            <button class="navbar-toggler fs-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="#navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-default">
-        <i class="bi-list"></i>
-    </span>
+            <button class="navbar-toggler fs-1" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02" aria-controls="#navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-default">
+                    <i class="bi-list"></i>
+                </span>
                 <span class="navbar-toggler-toggled">
-        <i class="bi-x"></i>
-    </span>
+                 <i class="bi-x"></i>
+                 </span>
             </button>
 
             <div class="collapse navbar-collapse align-items-center" id="navbarTogglerDemo02">
                 <ul class="navbar-nav  mb-2 mb-lg-0 mx-auto d-flex justify-content-center">
-                    <li class="nav-item my-2 my-lg-0">
-                        <a class="nav-link {{ request()->routeIs('guest.welcome') ? 'active' : '' }}" aria-current="page" href="{{route('guest.welcome')}}">HOME</a>
+                    <li class="nav-item my-1 my-lg-0">
+                        <a class="nav-link {{ request()->routeIs('guest.welcome') ? 'active' : '' }}"
+                           aria-current="page" href="{{route('guest.welcome')}}">HOME</a>
                     </li>
-                    <li class="nav-item my-2 my-lg-0">
-                        <a class="nav-link {{ request()->routeIs('guest.privacy-policy') ? 'active' : '' }}" href="{{route('guest.privacy-policy')}}">POLICIES</a>
+                    <li class="nav-item my-1 my-lg-0">
+                        <a class="nav-link {{ request()->routeIs('guest.privacy-policy') ? 'active' : '' }}"
+                           href="{{route('guest.privacy-policy')}}">POLICIES</a>
                     </li>
-                    <li class="nav-item my-2 my-lg-0">
-                        <a class="nav-link {{ request()->routeIs('guest.contact') ? 'active' : '' }}" href="{{route('guest.contact')}}" tabindex="-1">CONTACT US</a>
+                    <li class="nav-item my-1 my-lg-0">
+                        <a class="nav-link {{ request()->routeIs('guest.contact') ? 'active' : '' }}"
+                           href="{{route('guest.contact')}}" tabindex="-1">CONTACT US</a>
                     </li>
-                    <li class="nav-item my-2 my-lg-0">
-                        <a class="nav-link {{ request()->routeIs('guest.help') ? 'active' : '' }}" href="{{route('guest.help')}}" tabindex="-1">HELP</a>
+                    <li class="nav-item my-1 my-lg-0">
+                        <a class="nav-link {{ request()->routeIs('guest.help') ? 'active' : '' }}"
+                           href="{{route('guest.help')}}" tabindex="-1">HELP</a>
                     </li>
                 </ul>
                 <div class="d-block d-md-flex nav-buttons ms-3 ms-lg-0">
-                   @auth
+                    @auth
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-                            <button class="btn btn-outline btn-primary px-md-5 px-lg-4 px-xl-5 ms-2 text-white shadow-none text-uppercase" type="submit">Logout</button>
+                            <button
+                                class="btn btn-outline btn-primary px-md-5 px-lg-4 px-xl-5 ms-2 text-white shadow-none text-uppercase"
+                                type="submit">Logout
+                            </button>
                         </form>
                     @endauth
 
                     @guest
-                           <a href="{{route('register')}}" class="btn btn-outline-dark px-md-5 px-lg-4 px-xl-5">SIGN UP</a>
-                           <a href="{{route('login')}}" class="btn btn-outline btn-primary px-md-5 px-lg-4 px-xl-5 ms-2 text-white shadow-none">LOGIN</a>
+                        <a href="{{route('register')}}" class="btn btn-outline-dark px-md-5 px-lg-4 px-xl-5 py-2">SIGN
+                            UP</a>
+                        <a href="{{route('login')}}"
+                           class="btn btn-outline btn-primary px-md-5 px-lg-4 px-xl-5 py-2 ms-2 text-white shadow-none">LOGIN</a>
                     @endguest
                 </div>
             </div>
