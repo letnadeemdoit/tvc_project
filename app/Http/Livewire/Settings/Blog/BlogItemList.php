3a3f602f3ebfc5b08a713fb535da52f03c168cd3
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-08-11 12:27:09 +0500

    Push Latest Changes

---
+++ app/Http/Livewire/Settings/Blog/BlogItemList.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Livewire\Settings\Blog;
+
+use App\Http\Livewire\Traits\Destroyable;
+use App\Models\Blog\Blog;
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class BlogItemList extends Component
+{
+    use WithPagination;
+    use Destroyable;
+
+    public $user;
+
+    public $search = '';
+    public $page = 1;
+    public $per_page = 15;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'page' => ['except' => 1],
+        'per_page' => ['except' => 15],
+    ];
+
+    protected $paginationTheme = 'bootstrap';
+
+    protected $listeners = [
+        'destroyed-successfully' => '$refresh',
+        'user-cu-successfully' => '$refresh',
+    ];
+
+    public function mount()
+    {
+        $this->model = Blog::class;
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $data = Blog::where('HouseId', $this->user->HouseId)
+            ->when($this->search !== '', function ($query) {
+                $query->where(function ($query) {
+                    $query
+                        ->where('Subject', 'LIKE', "%$this->search%")
+                        ->orWhere('Author', 'LIKE', "%$this->search%")
+                        ->orWhere('Audit_FirstName', 'LIKE', "%$this->search%")
+                        ->orWhere('Audit_Email', 'LIKE', "%$this->search%");
+                });
+            })
+            ->orderBy('BlogId', 'DESC')
+            ->paginate($this->per_page);
+        return view('dash.settings.blog.blog-items-list', compact('data'));
+    }
+}
---
+++ app/Http/Livewire/Settings/Blog/CreateOrUpdateBlogItemForm.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Livewire\Settings\Blog;
+
+use App\Models\Blog\Blog;
+use App\Models\Blog\BlogComment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class CreateOrUpdateBlogItemForm extends Component
+{
+    use WithFileUploads;
+
+    public $user;
+
+    public $state = [];
+
+    public $file;
+
+    public ?Blog $blogItem;
+
+    protected $listeners = [
+        'showBlogCUModal',
+    ];
+
+    public function render()
+    {
+        return view('dash.settings.blog.create-or-update-blog-item-form');
+    }
+
+    public function hydrate()
+    {
+        $this->dispatchBrowserEvent('modal-is-shown');
+    }
+
+    public function showBlogCUModal($toggle, ?Blog $blogItem)
+    {
+//        dd($blogItem);
+        $this->emitSelf('toggle', $toggle);
+        $this->blogItem = $blogItem;
+        $this->reset(['state', 'file']);
+
+        if ($blogItem) {
+            $this->state = \Arr::only($blogItem->toArray(), ['Subject', 'Content', 'BlogImage']);
+        }
+    }
+
+    public function saveBlogItemCU()
+    {
+        $this->resetErrorBag();
+        $user = Auth::user();
+        $date = date('Y/m/d H:i:s');
+        $inputs = $this->state;
+
+        if ($this->file) {
+            $inputs['BlogImage'] = $this->file;
+        }else{
+            unset($inputs['BlogImage']);
+        }
+
+        Validator::make($inputs, [
+            'Subject' => 'required|string|max:100',
+            'BlogImage' => 'nullable|mimes:png,jpg,gif,tiff',
+            'Content' => 'required',
+        ])->validateWithBag('saveBlogItemCU');
+
+        $this->blogItem->fill([
+            'HouseId' => $user->HouseId,
+            'Subject' => $inputs['Subject'],
+            'Content' => $inputs['Content'],
+            'Author' => $user->first_name,
+            'BlogDate' => $date,
+            'Audit_user_name' => $user->Audit_user_name,
+            'Audit_Role' => $user->Audit_Role,
+            'Audit_FirstName' => $user->Audit_FirstName,
+            'Audit_LastName' => $user->Audit_LastName,
+            'Audit_Email' => $user->Audit_Email,
+        ])->save();
+
+        $this->blogItem->updateFile($this->file, 'BlogImage');
+
+        $this->emitSelf('toggle', false);
+        session()->flash('success', 'New Blog Comment Added successfully...');
+//        $this->emit('blog-cu-successfully');
+    }
+
+    public function updatedFile() {
+        $this->validateOnly('file', ['file' => 'required|mimes:png,jpg,gif,tiff']);
+    }
+
+    public function deleteFile() {
+        if ($this->blogItem->BlogId) {
+            $this->blogItem->deleteFile('BlogImage');
+            $this->emit('blog-cu-successfully');
+        }
+    }
+}
