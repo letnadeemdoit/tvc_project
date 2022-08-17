91b75fd5830a5452f95c3fa804baf44c08b256ce
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-08-17 18:47:03 +0500

    Push Latest Changes

--- resources/views/blog/like-able-blog.blade.php
+++ resources/views/blog/like-able-blog.blade.php
@@ -1,7 +1,7 @@
 <p class="ps-0">
 <a href="#!">
     @if($isExistingUser)
-    <img src="/images/blog-images/filled-heart.svg" class="img-fluid me-1">
+    <img src="/images/blog-images/filled-heart.svg" class="img-fluid me-1" wire:click="likeBlog()">
     @else
         <img src="/images/blog-images/love.png" class="img-fluid me-1" wire:click="likeBlog()">
     @endif
