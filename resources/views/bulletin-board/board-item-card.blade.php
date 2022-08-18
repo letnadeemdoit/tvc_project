e450dd0238a8897509f6ec30f4d1b2d0f4da37ef
Ahmad_Dev <105263889+Ahmad-DevDimension@users.noreply.github.com>
2022-08-18 15:00:51 +0500

    bulletin board...

--- resources/views/bulletin-board/board-item-card.blade.php
+++ resources/views/bulletin-board/board-item-card.blade.php
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="card mb-3">
     <img src="/images/bulletin-images/house-1.png" class="card-img-top" alt="..."/>
     <div class="card-body">
         <h3 class="card-title">{{$dt->title}}</h3>
