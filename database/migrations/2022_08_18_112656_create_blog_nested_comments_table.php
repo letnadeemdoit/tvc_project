c982b7503f5e581979847c24993ebf30c13e9a16
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-08-18 17:07:16 +0500

    Push Latest Changes

---
+++ database/migrations/2022_08_18_112656_create_blog_nested_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_nested_comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('blog_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('comment_id');
+            $table->string('author');
+            $table->text('nested_content');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_nested_comments');
+    }
+};
