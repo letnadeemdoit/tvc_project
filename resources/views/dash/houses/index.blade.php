a8f31fcdee68e2b0b5fbd35fa18533515ff5b516
Rana Tayyab <tayyab.devdi@gmail.com>
2022-08-22 14:09:45 +0500

    ....

--- resources/views/dash/houses/index.blade.php
+++ resources/views/dash/houses/index.blade.php
@@ -1,12 +1,10 @@
-<x-app-layout>
-    <x-dashboard-layout>
-        <x-slot name="breadcrumbs">
-            <li class="breadcrumb-item active" aria-current="page">Manage House</li>
-        </x-slot>
-        <x-slot name="title">
-            House
-        </x-slot>
+<x-dashboard-layout>
+    <x-slot name="breadcrumbs">
+        <li class="breadcrumb-item active" aria-current="page">Manage House</li>
+    </x-slot>
+    <x-slot name="title">
+        House
+    </x-slot>
 
 
-    </x-dashboard-layout>
-</x-app-layout>
+</x-dashboard-layout>
