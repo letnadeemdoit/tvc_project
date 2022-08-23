57b91ede959526636c067d55032c710725290355
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-08-23 17:08:13 +0500

    Push Latest Changes

--- resources/views/blog/latest-post.blade.php
+++ resources/views/blog/latest-post.blade.php
@@ -30,17 +30,12 @@
         @foreach($data as $dt)
         <div class="d-flex align-items-center mb-4">
             <div class="flex-shrink-0">
-{{--                <img--}}
-{{--                    class="avatar-img rounded-circle"--}}
-{{--                    src="{{ auth()->user()->first_name }}"--}}
-{{--                    :src="avatarUrl"--}}
-{{--                    alt="Image"--}}
-{{--                    width="60" height="60" style="object-fit: cover"--}}
-{{--                />--}}
                 <img src="/images/blog-images/rounded-image.png" class="img-fluid position-relative" alt="...">
             </div>
             <div class="flex-grow-1 ms-3">
+                <a href="{{route('guest.blog.show', $dt->slug)}}">
                 <h4 class="mb-0" style="color: #6D6D6D">{{ $dt->Subject }}</h4>
+                </a>
                 <p class="mb-0" style="color: #B6B4B4">{{\Carbon\Carbon::parse($dt->BlogDate)->format('d M Y')}}</p>
             </div>
         </div>
