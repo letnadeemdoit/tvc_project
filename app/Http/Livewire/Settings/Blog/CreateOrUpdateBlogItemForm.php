8c15fc8fad1415069d03366472236e9f7a5aeb72
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-08-31 11:53:33 +0500

    push changes

--- app/Http/Livewire/Settings/Blog/CreateOrUpdateBlogItemForm.php
+++ app/Http/Livewire/Settings/Blog/CreateOrUpdateBlogItemForm.php
@@ -69,7 +69,6 @@ class CreateOrUpdateBlogItemForm extends Component
 
     public function saveBlogItemCU()
     {
-        dd($this->name);
         $this->resetErrorBag();
         $date = date('Y/m/d H:i:s');
         $inputs = $this->state;
@@ -88,6 +87,7 @@ class CreateOrUpdateBlogItemForm extends Component
         ])->validateWithBag('saveBlogItemCU');
 
         $slug = Str::slug($inputs['Subject']);
+        dd($inputs['Contents']);
 
         $this->blogItem->fill([
             'HouseId' => $this->user->HouseId,
