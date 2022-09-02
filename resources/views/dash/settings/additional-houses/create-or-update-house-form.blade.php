00471f8ae3d5ce41b19f9f44c518b29844f09da7
Nouman Afzal <noumanafzal.devdimensions.com>
2022-09-02 17:28:52 +0500

    Adding a new house from an other admin didn't show my cropping image

--- resources/views/dash/settings/additional-houses/create-or-update-house-form.blade.php
+++ resources/views/dash/settings/additional-houses/create-or-update-house-form.blade.php
@@ -8,8 +8,16 @@
                 aria-label="Close"
                 @click.prevent="hide()"
             ></button>
+
+
         </div>
         <div class="modal-body">
+
+            @if($house && $house->HouseID)
+
+            @else
+                <p class="text-dark fw-600">Your current logged in account information will be used to create new administrator account for this new house</p>
+            @endif
             <form wire:submit.prevent="saveAdditionalHouseCU" method="post">
 
 
@@ -198,50 +206,119 @@
                     <span class="invalid-feedback">{{$message}}</span>
                     @enderror
                 </div>
-                <div class="row mb-3">
-                    <div class="col-md-4">
-                        <label class="form-label" for="city">City:</label>
-                        <input
-                            type="text"
-                            id="city"
-                            wire:model.defer="state.city"
-                            name="city"
-                            class="form-control @error('city') is-invalid @enderror"
-                            placeholder="City"
-                        />
-                        @error('city')
-                        <span class="invalid-feedback">{{$message}}</span>
-                        @enderror
+
+                <div class="row mt-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-6 mx-auto">
+                        <div class="form-group mb-3" wire:ignore>
+                            <label for="" class="form-label">Country</label>
+                            <select
+                                name="country_id"
+                                wire:model="state.country_id"
+                                class="form-control"
+                            >
+                                <option value="">--select country--</option>
+                                @foreach($countries as $country)
+                                    <option value="{{ $country->id }}">{{ $country->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label class="form-label" for="state">State:</label>
-                        <input
-                            type="text"
-                            id="state"
-                            wire:model.defer="state.state"
-                            name="state"
-                            class="form-control @error('state') is-invalid @enderror"
-                            placeholder="State"
-                        />
-                        @error('state')
-                        <span class="invalid-feedback">{{$message}}</span>
-                        @enderror
+
+                    <div class="col-12 col-md-6 col-lg-6 mx-auto">
+                        <div class="form-group mb-3 ">
+                            <label for="state_id" class="form-label">State</label>
+                            <select
+                                name="state_id"
+                                wire:model="state.state_id"
+                                class="form-control"
+                                data-live-search="true"
+                            >
+                                @foreach($states as $state)
+                                    <option value="{{ $state->id }}">{{ $state->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="col-md-4">
-                        <label class="form-label" for="zipcode">Zipcode:</label>
-                        <input
-                            type="text"
-                            id="zipcode"
-                            wire:model.defer="state.zipcode"
-                            name="zipcode"
-                            class="form-control @error('zipcode') is-invalid @enderror"
-                            placeholder="Zipcode"
-                        />
-                        @error('zipcode')
-                        <span class="invalid-feedback">{{$message}}</span>
-                        @enderror
+
+                    <div class="col-12 col-md-6 col-lg-6 mx-auto">
+                        <div class="form-group mb-3 ">
+                            <label for="city_id" class="form-label">City</label>
+                            <select
+                                name="city_id"
+                                wire:model.defer="state.city_id"
+                                class="form-control"
+                            >
+                                @foreach($cities as $city)
+                                    <option value="{{ $city->id }}">{{ $city->name }}</option>
+                                @endforeach
+                            </select>
+
+                        </div>
                     </div>
+
+                    <div class="col-12 col-md-6 col-lg-6 mx-auto">
+                        <div class="form-group mb-3 ">
+                            <label class="form-label" for="zipcode">Zipcode:</label>
+                            <input
+                                type="text"
+                                id="zipcode"
+                                wire:model.defer="state.zipcode"
+                                name="zipcode"
+                                class="form-control @error('zipcode') is-invalid @enderror"
+                                placeholder="Zipcode"
+                            />
+                            @error('zipcode')
+                            <span class="invalid-feedback">{{$message}}</span>
+                            @enderror
+                        </div>
+                    </div>
+
                 </div>
+
+{{--                <div class="row mb-3">--}}
+{{--                    <div class="col-md-4">--}}
+{{--                        <label class="form-label" for="city">City:</label>--}}
+{{--                        <input--}}
+{{--                            type="text"--}}
+{{--                            id="city"--}}
+{{--                            wire:model.defer="state.city"--}}
+{{--                            name="city"--}}
+{{--                            class="form-control @error('city') is-invalid @enderror"--}}
+{{--                            placeholder="City"--}}
+{{--                        />--}}
+{{--                        @error('city')--}}
+{{--                        <span class="invalid-feedback">{{$message}}</span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-4">--}}
+{{--                        <label class="form-label" for="state">State:</label>--}}
+{{--                        <input--}}
+{{--                            type="text"--}}
+{{--                            id="state"--}}
+{{--                            wire:model.defer="state.state"--}}
+{{--                            name="state"--}}
+{{--                            class="form-control @error('state') is-invalid @enderror"--}}
+{{--                            placeholder="State"--}}
+{{--                        />--}}
+{{--                        @error('state')--}}
+{{--                        <span class="invalid-feedback">{{$message}}</span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-4">--}}
+{{--                        <label class="form-label" for="zipcode">Zipcode:</label>--}}
+{{--                        <input--}}
+{{--                            type="text"--}}
+{{--                            id="zipcode"--}}
+{{--                            wire:model.defer="state.zipcode"--}}
+{{--                            name="zipcode"--}}
+{{--                            class="form-control @error('zipcode') is-invalid @enderror"--}}
+{{--                            placeholder="Zipcode"--}}
+{{--                        />--}}
+{{--                        @error('zipcode')--}}
+{{--                        <span class="invalid-feedback">{{$message}}</span>--}}
+{{--                        @enderror--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
                 <div class="mb-3 d-flex">
                     <button type="submit" class="btn btn-primary px-5 ms-auto">{{ $house && $house->HouseID ? "Update" : 'Add' }} House</button>
