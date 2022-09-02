8992172624308aa3996270fbdbeb983678a8b89b
Nouman Afzal <noumanafzal.devdimensions.com>
2022-09-02 14:34:30 +0500

    notification format

--- app/Notifications/CalendarEmailNotification.php
+++ app/Notifications/CalendarEmailNotification.php
@@ -55,7 +55,7 @@ class CalendarEmailNotification extends Notification
 
             ->greeting('Vacation Calendar')
 
-            ->line(new HtmlString('New Vacation <strong>' . $this->items->VacationName.'</strong>'. ' has been Schedule against ' . '<strong>'. $this->createdHouseName .' '.'House'.'</strong>'))
+            ->line(new HtmlString('New Vacation <strong>' . $this->items->VacationName.'</strong>'. ' has been Scheduled against ' . '<strong>'. $this->createdHouseName .' '.'House'.'</strong>'))
 
             ->line(new HtmlString('This Vacation has been Scheduled from <strong>' . $this->startDate->RealDate.'</strong>'. ' to ' . '<strong>'. $this->endDate->RealDate .' '.'Date'.'</strong>'));
 
