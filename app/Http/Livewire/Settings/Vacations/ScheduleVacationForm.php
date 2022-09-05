34b1957b6ea63264854fd65bd39ffabf1510d0dc
Nouman Afzal <noumanafzal.devdimensions.com>
2022-09-05 17:56:57 +0500

    calendar exception

--- app/Http/Livewire/Settings/Vacations/ScheduleVacationForm.php
+++ app/Http/Livewire/Settings/Vacations/ScheduleVacationForm.php
@@ -135,43 +135,33 @@ class ScheduleVacationForm extends Component
             'OwnerId' => $this->user->user_id,
         ])->save();
 
-
 //        if (!is_null($this->user->house->CalEmailList)){
-//
 //            $CalEmailList = explode(',',$this->user->house->CalEmailList);
-//
 //            if (count($CalEmailList) > 0) {
-//
 //                $users = User::whereIn('email', $CalEmailList)->where('HouseId', $this->user->HouseId)->get();
-//
 //                foreach ($users as $user) {
-//
 //                    $user->notify(new CalendarEmailNotification($items,$createdHouseName,$startDate,$endDate));
-//
 //                }
-//
 ////                Notification::send($users, new CalendarEmailNotification($items,$createdHouseName,$startDate,$endDate));
-//
 //                $CalEmailList = array_diff($CalEmailList, $users->pluck('email')->toArray());
-//
 //                if (count($CalEmailList) > 0) {
-//
 //                    Notification::route('mail', $CalEmailList)
 //                        ->notify(new CalendarEmailNotification($items,$createdHouseName,$startDate,$endDate));
-//
 //                }
 //            }
-//
 //        }
-
 
         try {
 
             $items =$this->vacation;
             $createdHouseName = $this->user->house->HouseName;
+
             if (!is_null($this->user->house->CalEmailList) && !empty($this->user->house->CalEmailList)){
+
                 $CalEmailList = explode(',',$this->user->house->CalEmailList);
+
                 if (count($CalEmailList) > 0 && !empty($CalEmailList)){
+
                     $users = User::whereIn('email', $CalEmailList)->where('HouseId', $this->user->HouseId)->get();
                     foreach ($users as $user) {
                         $user->notify(new CalendarEmailNotification($items,$createdHouseName,$startDate,$endDate));
@@ -181,6 +171,7 @@ class ScheduleVacationForm extends Component
                         Notification::route('mail', $CalEmailList)
                             ->notify(new CalendarEmailNotification($items,$createdHouseName,$startDate,$endDate));
                     }
+
                 }
             }
 
