186552b6c54449f66cc343c291bba6c7b6526026
Rana Tayyab <tayyab.devdi@gmail.com>
2022-09-08 14:59:17 +0500

    ...

--- app/Models/ICal.php
+++ app/Models/ICal.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Eluceo\iCal\Domain\ValueObject\MultiDay;
+use Eluceo\iCal\Domain\ValueObject\SingleDay;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -38,5 +40,33 @@ class ICal extends Model
     public function houseVacations()
     {
         return $this->hasMany(Vacation::class, 'HouseId', 'house_id');
+    }
+
+    public function toICSUrl() {
+
+        $links = [];
+
+        $vacations = $this->user->is_admin ? $this->houseVacations : $this->userVacations;
+
+        foreach ($vacations as $vacation) {
+            $from = $vacation->startDatetime->format('Y-m-d H:i:s');
+            $to = $vacation->endDatetime->format('Y-m-d H:i:s');
+
+            $from = \DateTime::createFromFormat('Y-m-d H:i:s', $from);
+            $to = \DateTime::createFromFormat('Y-m-d H:i:s', $to);
+
+            // this is the new library which we are trying
+            $links[] = (new \Eluceo\iCal\Domain\Entity\Event())
+                ->setSummary($vacation->VacationName)
+//                ->setDescription('Location: ' . $vacation->DisplayIMR . ', More Information: ' . $vacation->FullURLtoEventDashboard)
+                ->setOccurrence(new MultiDay(new \Eluceo\iCal\Domain\ValueObject\Date($from), new \Eluceo\iCal\Domain\ValueObject\Date($to)));
+        }
+
+        // this is the new library which we are trying
+        $calendar = new \Eluceo\iCal\Domain\Entity\Calendar($links);
+
+        // this is the new library which we are trying
+        $componentFactory = new \Eluceo\iCal\Presentation\Factory\CalendarFactory();
+        return $componentFactory->createCalendar($calendar);
     }
 }
