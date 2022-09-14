143d031e055da513174a0db96c4c6db2a31366ce
Nouman Afzal <noumanafzal.devdimensions.com>
2022-09-14 15:34:26 +0500

    dynamic addresses

--- app/Http/Livewire/Settings/HouseSettings/HouseDetails.php
+++ app/Http/Livewire/Settings/HouseSettings/HouseDetails.php
@@ -42,12 +42,6 @@ class HouseDetails extends Component
         return view('dash.settings.house-settings.house-details',compact('countries','states','cities'));
     }
 
-//    public function updatedState()
-//    {
-//        $this->state['state_id'] = '';
-//        $this->state['city_id'] = '';
-//    }
-
     public function mount(){
 
         $this->house = $this->user->house;
@@ -84,8 +78,6 @@ class HouseDetails extends Component
             unset($inputs['image']);
         }
 
-
-
         Validator::make($inputs, [
             'image' => 'nullable|mimes:png,jpg,gif,tiff',
             'primary_house_name' => ['nullable', 'string', 'max:100'],
@@ -107,8 +99,6 @@ class HouseDetails extends Component
         $country_name = Country::where('id',$inputs['country_id'])->first();
         $state_name = State::where('id',$inputs['state_id'])->first();
         $city_name = City::where('id',$inputs['city_id'])->first();
-
-//        dd($country_name['name'] . ' ' . $state_name['name']. ' '. $city_name['name']);
 
         $this->house->fill([
             'HouseName' => $this->state['name'],
