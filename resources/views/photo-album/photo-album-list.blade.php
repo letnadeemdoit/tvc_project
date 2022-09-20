33060d00d541773aa48b5a542b9472e269b766da
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-09-20 17:28:04 +0500

    resolve clickup issue

--- resources/views/photo-album/photo-album-list.blade.php
+++ resources/views/photo-album/photo-album-list.blade.php
@@ -1,6 +1,7 @@
 <div class="container padding-bottom massonary-container">
     <div class="masonry">
-        @foreach($data as $dt)
+        @if(isset($data) && count($data) > 0)
+            @foreach($data as $dt)
             @if($dt instanceof \App\Models\Photo\Album && ($dt->nestedAlbums->count() > 0 or $dt->photos->count() > 0))
                 <div class="brick">
                     <livewire:photo-album.album-card :album="$dt" wire:key="{{ $dt->id }}" />
@@ -11,6 +12,9 @@
                 </div>
             @endif
         @endforeach
+        @else
+            @include('partials.no-data-available',['title' => 'Album'])
+        @endif
     </div>
 
     @push('scripts')
