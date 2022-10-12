cac580516ea20d875d92547cc41eaee4a162569b
Nouman Afzal <noumanafzal.devdimensions.com>
2022-10-12 19:08:14 +0500

    audit

--- resources/views/layouts/partials/navigation-menu-top-app.blade.php
+++ resources/views/layouts/partials/navigation-menu-top-app.blade.php
@@ -10,7 +10,7 @@
 
                         <li class="nav-item my-1 my-lg-0">
                             <a class="nav-link fw-500"
-                               href="{{route('dash.calendar')}}">Dashboard</a>
+                               href="{{route('dash.calendar')}}">Calendar</a>
                         </li>
 
                         <li class="nav-item my-1 my-lg-0">
