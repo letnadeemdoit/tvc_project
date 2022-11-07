b59aac2c88326b39a2dc0d1a5f4137b952d8983f
Nouman Afzal <noumanafzal.devdimensions.com>
2022-11-07 11:59:55 +0500

    https://app.clickup.com/t/3rpm3pu

--- resources/views/layouts/partials/navigation-menu-top-app.blade.php
+++ resources/views/layouts/partials/navigation-menu-top-app.blade.php
@@ -36,25 +36,26 @@
             <!-- End Navbar Vertical Toggle -->
         </div>
 
+        @if(auth()->user()->is_admin)
+            @if(auth()->user()->house->primary_house_name !== null and auth()->user()->house->primary_house_name !== '')
+            @else
 
-        @if(auth()->user()->house->primary_house_name !== null and auth()->user()->house->primary_house_name !== '')
-        @else
+                <div class="d-flex justify-content-start d-none d-lg-block">
+    {{--                <span class="fw-semi-bold text-primary">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}!</span>--}}
+                    <form method="POST" action="{{ route('dash.switch-house') }}">
+                    @method('PUT')
+                    @csrf
 
-            <div class="d-flex justify-content-start d-none d-lg-block">
-{{--                <span class="fw-semi-bold text-primary">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}!</span>--}}
-                <form method="POST" action="{{ route('dash.switch-house') }}">
-                @method('PUT')
-                @csrf
+                    <!-- Hidden Team ID -->
+                        <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
+                        <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-12">
+                            You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
+                            <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
+                        </button>
+                    </form>
+                </div>
 
-                <!-- Hidden Team ID -->
-                    <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
-                    <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-12">
-                        You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
-                        <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
-                    </button>
-                </form>
-            </div>
-
+            @endif
         @endif
 
 
@@ -332,25 +333,26 @@
             </ul>
         </div>
     </nav>
+    @if(auth()->user()->is_admin)
+        @if(auth()->user()->house->primary_house_name !== null and auth()->user()->house->primary_house_name !== '')
+        @else
 
-    @if(auth()->user()->house->primary_house_name !== null and auth()->user()->house->primary_house_name !== '')
-    @else
+            <div class="d-block d-lg-none">
+                {{--                <span class="fw-semi-bold text-primary">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}!</span>--}}
+                <form method="POST" class="text-center mx-auto" action="{{ route('dash.switch-house') }}">
+                @method('PUT')
+                @csrf
 
-        <div class="d-block d-lg-none">
-            {{--                <span class="fw-semi-bold text-primary">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}!</span>--}}
-            <form method="POST" class="text-center mx-auto" action="{{ route('dash.switch-house') }}">
-            @method('PUT')
-            @csrf
+                <!-- Hidden Team ID -->
+                    <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
+                    <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-11">
+                        You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
+                        <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
+                    </button>
+                </form>
+            </div>
 
-            <!-- Hidden Team ID -->
-                <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
-                <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-11">
-                    You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
-                    <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
-                </button>
-            </form>
-        </div>
-
+        @endif
     @endif
 
 </header>
