ec3a444cb86b12b32e71ee913f866509e81bb5f5
Nouman Afzal <noumanafzal.devdimensions.com>
2022-11-07 12:02:10 +0500

    https://app.clickup.com/t/3rpm3pu

--- resources/views/layouts/partials/navigation-menu-top-app.blade.php
+++ resources/views/layouts/partials/navigation-menu-top-app.blade.php
@@ -47,7 +47,7 @@
                     @csrf
 
                     <!-- Hidden Team ID -->
-                        <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
+                        <input type="hidden" name="house_id" value="{{ primary_user()->HouseId }}">
                         <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-12">
                             You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
                             <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
@@ -344,7 +344,7 @@
                 @csrf
 
                 <!-- Hidden Team ID -->
-                    <input type="hidden" name="house_id" value="{{ primary_user()->HouseID }}">
+                    <input type="hidden" name="house_id" value="{{ primary_user()->HouseId }}">
                     <button type="submit" class="bg-transparent border-0 text-light-grey fw-500 fs-11">
                         You are login as a <span class="fw-600 text-primary">{{ auth()->user()->house->HouseName }}</span> house
                         <Span class="text-decoration-underline text-primary">Click to go Primary house</Span>
