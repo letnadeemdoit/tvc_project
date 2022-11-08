324cb1545c420ece067cbee107464cc47e26eaa4
Nadeem Ahmad <nadeem.ahmad@devdimensions.com>
2022-11-08 17:09:08 +0500

    Worked on paypal status update method

---
+++ app/Http/Middleware/CheckSubscriptionStatusOnPaypal.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Subscription;
+use Closure;
+use Illuminate\Http\Request;
+use Srmklive\PayPal\Facades\PayPal;
+
+class CheckSubscriptionStatusOnPaypal
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->check()){
+
+            $paypal = PayPal::setProvider();
+            $paypal->getAccessToken();
+            $user_id = auth()->user()->user_id;
+
+            $current_subscription = Subscription::where('user_id', $user_id)
+                ->whereIn('status', ['APPROVAL_PENDING','ACTIVE'])
+                ->latest()
+                ->first();
+
+            if ($current_subscription){
+                $paypalSubscription = $paypal->showSubscriptionDetails($current_subscription->subscription_id);
+                Subscription::where('user_id', $user_id)->where('subscription_id', $current_subscription->subscription_id)->update([
+                    'status' => $paypalSubscription['status']
+                ]);
+            }
+
+        }
+        return $next($request);
+    }
+}
