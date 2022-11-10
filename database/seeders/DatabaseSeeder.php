12f7583c456df710ed7ac94750986fbde6756af0
Rana Tayyab <tayyab.devdi@gmail.com>
2022-11-10 16:05:13 +0500

    fixes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            BedTypesTableSeeder::class,
+            CategorySeeder::class,
+            LocalGuideCategorySeeder::class,
+            SuperAdminTableSeeder::class,
+            ReducePhotosSizeAndGenerateThumbnailSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
