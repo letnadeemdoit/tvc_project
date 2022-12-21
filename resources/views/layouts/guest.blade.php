3c35867a9d5a83b76c65845687645af23e0006c2
Nadeem Ahmad <nadeem.ahmad+gitlab@devdimensions.com>
2022-12-21 16:30:13 +0500

    Worked on VC issues List

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,9 @@
 
             gtag('config', 'G-TXFZXTWXFP');
         </script>
+
+
+
         <!-- Start of HubSpot Embed Code -->
         <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/23120175.js"></script>
         <!-- End of HubSpot Embed Code -->
@@ -94,6 +97,25 @@
 
     </head>
     <body>
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '670856764739701');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="https://www.facebook.com/tr?id=670856764739701&ev=PageView&noscript=1"
+        />
+    </noscript>
+    <!-- End Meta Pixel Code -->
 {{--    <div id="fb-root"></div>--}}
         <main class="bg-lightGrey" style="min-height: 100vh">
             @include('layouts.partials.navigation-menu-top-guest')
