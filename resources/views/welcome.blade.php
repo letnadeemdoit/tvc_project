be9c80862bd1b2eb58b92f32e5e19c9079c5fcd2
Nadeem Ahmad <nadeem.ahmad+gitlab@devdimensions.com>
2023-01-04 16:05:51 +0500

    Worked on clickup issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -673,7 +673,7 @@
 
                                                     <div class="ps-3">
                                                         <h5 class="mb-1 fw-bold"
-                                                            style="color: #2A3342">{{ Str::upper('By '.$blog->user->first_name. ' ' .$blog->user->last_name[0] ?? '')  }}</h5>
+                                                            style="color: #2A3342">{{ Str::upper('By '.$blog->user->first_name. ' ' .substr($blog->user->last_name, 0, 1))  }}</h5>
                                                         <p class="mb-0 fs-13 txt-clr">{{\Carbon\Carbon::parse($blog->BlogDate)->format('d M Y')}}</p>
                                                     </div>
                                                 </div>
