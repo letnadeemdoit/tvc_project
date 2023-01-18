bc449487591750a3d4342d9fb8189c2125458a72
Nadeem Ahmad <nadeem.ahmad+gitlab@devdimensions.com>
2023-01-18 10:45:58 +0500

    Rooom feature implementation

--- resources/views/dash/plans-and-pricing/plans-and-pricing-list.blade.php
+++ resources/views/dash/plans-and-pricing/plans-and-pricing-list.blade.php
@@ -52,7 +52,7 @@
 {{--                            @else--}}
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >
                                     Unsubscribe
@@ -112,7 +112,7 @@
                             @else
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >
                                     Unsubscribe
@@ -187,7 +187,7 @@
                             @else
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >Unsubscribe</a>
                             @endif
@@ -252,7 +252,7 @@
                             @else
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >Unsubscribe</a>
                             @endif
@@ -317,7 +317,7 @@
                             @else
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >Unsubscribe</a>
                             @endif
@@ -399,7 +399,7 @@
                             @else
                                 <a
                                     href="#!"
-                                    wire:click.prevent="cancelSubscription"
+                                    data-bs-toggle="modal" data-bs-target="#unsubscribeModal"
                                     class="form-check-select-stretched-btn btn btn-primary"
                                 >Unsubscribe</a>
                             @endif
@@ -420,6 +420,43 @@
         <!-- End Col -->
 
     </div>
+
+        <div class="modal fade" id="unsubscribeModal" tabindex="-1" aria-labelledby="unsubscribeModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <h6 class="modal-title fs-10 text-white"
+                        id="deleteConfirmationModalLabel">{{ 'Delete!'}}</h6>
+                    <div class="modal-body text-center">
+                        <div>
+              <span class="rounded-circle text-primary border-primary" style="padding: 4px 9px; font-size: 26px; line-height: 75px;border: 3px solid;">
+                    <i class="bi-exclamation"></i>
+                </span>
+                        </div>
+
+                        <h4 class="fw-bold text-center my-3"
+                            style="color: #00000090">Are you sure to unsubscribe subscription.!</h4>
+                        <p class="fw-500 fs-15">You would not be able to recover this!</p>
+                        <div class="btn-group my-2">
+                            <button type="button"
+                                    class="btn px-5 btn-dark fw-500 text-uppercase fs-16 mb-2 mb-lg-0 w-180 mx-2 rounded py-2"
+                                    data-bs-dismiss="modal">Cancel
+                            </button>
+                            <button type="button"
+                                    class="btn btn-primary fw-500 text-uppercase fs-16 mb-2 mb-lg-0 w-180 mx-2 rounded py-2"
+                                    wire:click.prevent="cancelSubscription">
+                                <div wire:loading.remove wire:target="cancelSubscription">
+                                    Yes,Unsubscribe!
+                                </div>
+                                <div wire:loading wire:target="cancelSubscription">
+                                    Unsubscribing...
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </div>
 
 @push('scripts')
