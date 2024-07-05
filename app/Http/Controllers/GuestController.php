7b4e43d96b8948bd41cf0933f7eef46aa4b2a57e
Nadeem Ahmed <letnadeemdoit@gmail.com>
2024-07-05 17:27:56 +0500

    contact us email

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -71,23 +71,23 @@ class GuestController extends Controller
         $email = $request->email;
         $comment = $request->comment;
 
-        Notification::route('mail', $request->email)
-            ->notify(new ContactUsMailNotification($firstName, $lastName, $subject, $email, $comment));
-
-        Mail::send([], [], function ($message) use ($request) {
-
-            $message->to('support@thevacationcalendar.com')
-                ->subject($request->first_name . ' ' . 'Contact Query')
-                ->Html(
-                    '<div style="padding: 10px; 20px">' .
-                    '<h2> Contact Us Notification</h2>' .
-                    '<p> Name: ' . $request->first_name . ' ' . $request->last_name . '</p>' .
-                    '<p> Email: ' . $request->email . '<p/>' .
-                    '<p> Subject: ' . $request->subject . '<p/>' .
-                    '<p> Comment: ' . $request->comment . '<p/>' . '</br>' .
-                    '</div>', 'text/plain');
-//            $message->from($request->email, $request->first_name. ' ' . $request->last_name);
-        });
+//        Notification::route('mail', $request->email)
+//            ->notify(new ContactUsMailNotification($firstName, $lastName, $subject, $email, $comment));
+//
+//        Mail::send([], [], function ($message) use ($request) {
+//
+//            $message->to('support@thevacationcalendar.com')
+//                ->subject($request->first_name . ' ' . 'Contact Query')
+//                ->Html(
+//                    '<div style="padding: 10px; 20px">' .
+//                    '<h2> Contact Us Notification</h2>' .
+//                    '<p> Name: ' . $request->first_name . ' ' . $request->last_name . '</p>' .
+//                    '<p> Email: ' . $request->email . '<p/>' .
+//                    '<p> Subject: ' . $request->subject . '<p/>' .
+//                    '<p> Comment: ' . $request->comment . '<p/>' . '</br>' .
+//                    '</div>', 'text/plain');
+////            $message->from($request->email, $request->first_name. ' ' . $request->last_name);
+//        });
 
 
 
